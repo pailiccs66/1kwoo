caca5ddf9e454726e8c9e4c76552820e7e15c7be
oubaiwei <oubaiwei@example.com>
2026-06-11 02:19:25 +0800

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/1kwoo/' : '/',
   build: {
     sourcemap: 'hidden',
   },
